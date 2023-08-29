4c316c420172e24148f5ff7dd3722bcaafc6e91d
Adopolis23 <brandonw406@gmail.com>
2023-08-28 22:29:37 -0400

    Working pytorch convmixer code

--- pytorch/mnist_train.py
+++ pytorch/mnist_train.py
@@ -9,14 +9,32 @@ import copy
 import numpy as np
 import pandas as pd
 
+
+
+
 from pytorch_models import create_lenet
+from pytorch_models import ConvMixer
+
 
 
 
 batch_size = 64
-epochs = 3
-learning_rate = 0.001
-device = "cpu"
+epochs = 100
+learning_rate = 0.0001
+
+
+
+
+#check for gpu availability
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("CUDA Device Detected: {}".format(str(device)))
+else:
+    device = torch.device("cpu")
+    print("No Cuda Available")
+device
+
+
 
 #create the transorm to_tensor
 T = torchvision.transforms.Compose([
@@ -25,8 +43,8 @@ T = torchvision.transforms.Compose([
 
 
 #download the data
-train_data = torchvision.datasets.MNIST('mnist_data', train=True, download=True, transform=T)
-val_data = torchvision.datasets.MNIST('mnist_data', train=False, download=True, transform=T)
+train_data = torchvision.datasets.CIFAR10('cifar10_data', train=True, download=True, transform=T)
+val_data = torchvision.datasets.CIFAR10('cifar10_data', train=False, download=True, transform=T)
 
 
 #create the data loader
@@ -41,7 +59,7 @@ def validate(model, data):
     correct = 0
 
     for i, (images, labels) in enumerate(data):
-        #images = images.cuda()
+        images = images.cuda()
         x = model(images)
         value, pred = torch.max(x,1)
         pred = pred.data.cpu()
@@ -57,7 +75,10 @@ def train(numb_epoch=3, lr=1e-3, device="cpu"):
     accuracies = []
 
     #create model
-    cnn = create_lenet().to(device)
+    #cnn = create_lenet().to(device)
+
+    #91.26 expected accuracy
+    cnn = ConvMixer(dim=256, depth=8, patch_size=8, kernel_size=8, n_classes=10).to(device)
 
     #loss function
     cec = nn.CrossEntropyLoss()
@@ -70,8 +91,9 @@ def train(numb_epoch=3, lr=1e-3, device="cpu"):
     #main training loop
     for epoch in range(numb_epoch):
 
-        #for each image in the training dataset
+        #for each batch in the training dataset
         for i, (images, labels) in enumerate(train_dl):
+            #print("Training batch number {}".format(i))
 
             #send images and labels to device
             images = images.to(device)
@@ -103,11 +125,12 @@ def train(numb_epoch=3, lr=1e-3, device="cpu"):
 
     #plot accuracies and save best weights
     plt.plot(accuracies)
+    plt.show()
     return best_model
 
-#print("Starting Training")
-#best_model = train(epochs, learning_rate, device)
-#print("Training End")
+print("Starting Training")
+best_model = train(epochs, learning_rate, device)
+print("Training End")
 
-print("Total examples in train_data is: {}".format(len(train_data)))
-print("Total examples in train_dl is: {}".format(len(train_dl)))
+#print("Total examples in train_data is: {}".format(len(train_data)))
+#print("Total examples in train_dl is: {}".format(len(train_dl)))
