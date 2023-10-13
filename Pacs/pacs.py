26b9337e2dbeaf6dc6e6ae38a9c7d3c0cbb55728
Adopolis23 <brandonw406@gmail.com>
2023-10-12 21:17:03 -0400

    Working pacs model

---
+++ Pacs/pacs.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+import cv2
+import random
+import time
+import gc
+
+from pacs_data_loader import LoadPacsData
+from ModelGen import Generate_Model_2
+
+
+learning_rate = 0.0001
+epochs = 3
+batch_size = 8
+num_classes = 7
+
+
+image_size = (227, 227)
+image_shape = (227, 227, 3)
+
+
+train_x, train_y, val_x, val_y, test_seen_x, test_seen_y, test_unseen_x, test_unseen_y = LoadPacsData()
+
+print("Train length = {}".format(len(train_x)))
+print("Val length = {}".format(len(val_x)))
+print("Test_seen length = {}".format(len(test_seen_x)))
+print("Test_unseen length = {}".format(len(test_unseen_x)))
+
+
+
+
+model = Generate_Model_2(num_classes, image_shape)
+print(model.summary())
+
+
+
+
+
+def validate():
+  y_pred = model.predict(val_x, verbose=0)
+  bce = tf.keras.losses.BinaryCrossentropy(from_logits=False)
+  val_loss = bce(val_y, y_pred).numpy()
+  return val_loss
+
+
+
+
+class checkpoint(tf.keras.callbacks.Callback):
+
+  def __init__(self):
+    self.min_loss = 1000000
+    self.min_weight = None
+
+  def on_epoch_end(self, epoch, logs=None):
+    val_loss = validate()
+ 
+  
+    if val_loss < self.min_loss:
+        print("\nValidation loss improved saving weights\n")
+        self.min_loss = val_loss
+        self.min_weight = model.get_weights()
+
+  def on_train_end(self, logs=None):
+    print("\nSetting new model weights.\n")
+    model.set_weights(self.min_weight)
+
+
+
+
+
+#SGD optimizer with learning rate and 0.9 momentum
+opt = tf.keras.optimizers.Adam(learning_rate=learning_rate) 
+
+#compile model with accuracy metric
+model.compile(loss="categorical_crossentropy",
+              optimizer=opt,
+              metrics=['accuracy'])
+
+
+
+start_time = time.time()
+
+#train the model
+model.fit(x=np.array(train_x, np.float32),
+            y=np.array(train_y, np.float32),
+            validation_data=(val_x, val_y),
+              batch_size=batch_size,
+              epochs=epochs,
+              shuffle=True,
+              callbacks=[checkpoint()])
+
+elapsed_time = time.time() - start_time
+
+print("Total training time: {}".format(elapsed_time))
+
+gc.collect()
+
+#model evaluation
+scores = model.evaluate(test_seen_x, test_seen_y, verbose=1, batch_size=batch_size)
+print('Test loss seen:', scores[0])
+print('Test accuracy seen:', scores[1])
+
+gc.collect()
+
+#model evaluation
+scores_unseen = model.evaluate(test_unseen_x, test_unseen_y, verbose=1, batch_size=batch_size)
+print('Test loss unseen:', scores_unseen[0])
+print('Test accuracy unseen:', scores_unseen[1])
---
+++ Pacs/pacs_data_loader.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import cv2
+import random
+
+
+
+def LoadPacsData(val_size_percent = 0.1, test_seen_size_percent = 0.1):
+
+    data_folder = "data/"
+
+    domains = ["art_painting/", "cartoon/", "photo/", "sketch/"]
+    leave_out_domain = ["photo/"]
+
+    classes = ["dog/", "elephant/", "giraffe/", "guitar/", "horse/", "house/", "person/"]
+
+    train_x = []
+    train_y = []
+
+    val_x = []
+    val_y = []
+
+    test_seen_x = []
+    test_seen_y = []
+
+    test_unseen_x = []
+    test_unseen_y = []
+
+
+    #load in images
+    for domain in domains:
+        print("Loading domain: {}".format(domain))
+
+        for i, _class in enumerate(classes):
+
+            #load the unseed domain into test_unseen
+            if domain in leave_out_domain:
+                files = os.listdir(data_folder + domain + _class)
+                
+                for file in files:
+
+                    image = cv2.imread(data_folder + domain + _class + file)
+                    #image=cv2.resize(image, image_size,interpolation = cv2.INTER_AREA)
+                    image=np.array(image)
+                    image = image.astype('float32')
+                    image /= 255 
+                    test_unseen_x.append(image)
+
+                    one_hot_y = [0, 0, 0, 0, 0, 0, 0]
+                    one_hot_y[i] += 1
+
+                    test_unseen_y.append(one_hot_y)
+
+                continue
+
+
+            #load the seen data inso training
+            files = os.listdir(data_folder + domain + _class)
+            
+            for file in files:
+
+                image = cv2.imread(data_folder + domain + _class + file)
+                #image=cv2.resize(image, image_size,interpolation = cv2.INTER_AREA)
+                image=np.array(image)
+                image = image.astype('float32')
+                image /= 255 
+                train_x.append(image)
+
+                one_hot_y = [0, 0, 0, 0, 0, 0, 0]
+                one_hot_y[i] += 1
+
+                train_y.append(one_hot_y)
+
+
+
+    #randomly take examples from training and seperate them into val and test_seen
+    val_size = int(val_size_percent * len(train_x))
+    test_seen_size = int(test_seen_size_percent * len(train_x))
+
+
+    for i in range(val_size):
+        x = random.randrange(0, len(train_x), 1)
+
+        val_x.append(train_x.pop(x))
+        val_y.append(train_y.pop(x))
+
+    for i in range(test_seen_size):
+        x = random.randrange(0, len(train_x), 1)
+
+        test_seen_x.append(train_x.pop(x))
+        test_seen_y.append(train_y.pop(x))
+
+    train_y = np.asarray(train_y).reshape(-1, 7)
+    train_x = np.asarray(train_x)
+
+    val_y = np.asarray(val_y).reshape(-1, 7)
+    val_x = np.asarray(val_x)
+
+    test_seen_y = np.asarray(test_seen_y).reshape(-1, 7)
+    test_seen_x = np.asarray(test_seen_x)
+
+    test_unseen_y = np.asarray(test_unseen_y).reshape(-1, 7)
+    test_unseen_x = np.asarray(test_unseen_x)
+
+    return train_x, train_y, val_x, val_y, test_seen_x, test_seen_y, test_unseen_x, test_unseen_y
+
+      
+
+
+
